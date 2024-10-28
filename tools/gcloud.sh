fa18a7f3664c4a6a71939a4eafde7f0faac17836
Kegan Maher <kegan.maher@gmail.com>
2024-10-27 23:31:50 -0700

    fix(gcloud): reset for idempotent install on linux
    delete keyring, don't append (instead overwrite) source list
    
    skip input on install

--- tools/gcloud.sh
+++ tools/gcloud.sh
@@ -4,7 +4,8 @@ if [[ "$OSTYPE" =~ "darwin" ]]; then
     brew install google-cloud-sdk
 elif [[ "$OSTYPE" =~ "linux" ]]; then
     sudo apt update && sudo apt install -qq apt-transport-https ca-certificates gnupg curl
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    sudo apt update && sudo apt install -qq google-cloud-cli
+    sudo rm -f /usr/share/keyrings/cloud.google.gpg
+    curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+    sudo apt update && sudo apt install -yqq google-cloud-cli
 fi
