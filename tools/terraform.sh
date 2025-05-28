bfab3b60793f877010a12bcc4c215edab723baf1
Kegan Maher <kegan@compiler.la>
2025-05-28 00:51:10 -0700

    feat(terraform): support MacOS install via brew

--- tools/terraform.sh
+++ tools/terraform.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-if [[ "$OSTYPE" =~ "linux" ]]; then
+if [[ "$OSTYPE" =~ "darwin" ]]; then
+    brew tap hashicorp/tap
+    brew install hashicorp/tap/terraform
+elif [[ "$OSTYPE" =~ "linux" ]]; then
     sudo apt update && sudo apt install -qq wget
     wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
